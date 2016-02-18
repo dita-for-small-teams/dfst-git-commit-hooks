f82588a2039c67ba7d2fdb2f5a4e068a78ed7fa0
Eliot Kimber <drmacro@yahoo.com>
2016-02-17 21:50:49 -0600

    Ruby require statements appear to not fail

---
+++ server-side/updateBaseXOnReceive.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+# 
+# Replicates the received commit in the database
+#
+# 
+#
+
+require "./BaseXClient"
+
+class BasexUpdater
+  def initialize()
+  end
+end
+
+   # If database does not exist, create it
+   
+   # Load changes to BaseX
+   
+   # Record git state details.
+   
+
+# 
+# End of script
+#   
